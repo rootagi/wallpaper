8b169bbd16ee2b678bfe2a1d889b65f1952586ae
AGI <248829238+rootagi@users.noreply.github.com>
2026-08-13 07:22:08 +0530

    Update site URL to use repository owner dynamically

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const owner = process.env.GITHUB_REPOSITORY_OWNER || 'rootagi';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mylinuxforwork.github.io',
+  site: `https://${owner}.github.io`,
   base: '/wallpaper',
   integrations: [tailwind()],
   build: {
